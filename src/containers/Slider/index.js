82fd8ed092173e26542c238c0ff47677366b719c
Laurent De La Calle <laurentdelacalle@hotmail.com>
2023-12-01 22:53:51 +0100

    tous les test ok

---
+++ src/containers/Slider/index.js
@@ -0,0 +1,70 @@
+
+import React, { useEffect, useState } from "react";
+import { useData } from "../../contexts/DataContext";
+import { getMonth } from "../../helpers/Date";
+import "./style.scss";
+
+const Slider = () => {
+  
+const { data } = useData();
+
+  
+  const [index, setIndex] = useState(0);
+  const byDateDesc = data?.focus.sort((evtA, evtB) =>
+    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+  );
+ 
+ // création d'un useffect plus propre
+  useEffect(() => {
+    const timerId = setTimeout(
+      // ajout du -1 pour corriger le slide blanc
+      () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
+      5000
+    );
+
+    return () => clearTimeout(timerId)
+    
+  },[index, byDateDesc]);
+
+  return (
+    <div className="SlideCardList">
+      {byDateDesc?.map((event, idx) => (
+        
+          <div
+            key={event.title}
+            className={`SlideCard SlideCard--${
+              index === idx ? "display" : "hide"
+            }`}
+          >
+            <img src={event.cover} alt="forum" />
+            <div className="SlideCard__descriptionContainer">
+              <div className="SlideCard__description">
+                <h3>{event.title}</h3>
+                <p>{event.description}</p>
+                <div>{getMonth(new Date(event.date))}</div>
+              </div>
+            </div>
+          </div>
+
+      ))}
+          {/* separation des map imbriqué pour résoudre le pb de d'id */}
+
+          <div className="SlideCard__paginationContainer">
+            <div className="SlideCard__pagination">
+              {byDateDesc?.map((event, radioIdx) => (
+                <input
+                  key={`${event.id}`}
+                  type="radio"
+                  name="radio-button"
+                  // remplacement idx par index
+                  checked={index === radioIdx}
+                  readOnly
+                />
+              ))}
+            </div>
+          </div>
+    </div>
+  );
+};
+
+export default Slider;
