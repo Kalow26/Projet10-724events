c4730243c146e476ea545a201dba8a0b0b5bfa48
Laurent De La Calle <laurentdelacalle@hotmail.com>
2023-12-04 10:06:07 +0100

    correction liens navbar + bug carroussel

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -15,6 +15,7 @@ import { useData } from "../../contexts/DataContext";
 const Page = () => {
   const {data} = useData()
   // récupération du dernier élément du tableau
+  // ajout des id sur les containers pour la navigation
   const lastEvent = data?.events.slice(-1)[0]
   return <>
     <header>
@@ -24,7 +25,7 @@ const Page = () => {
       <section className="SliderContainer">
         <Slider />
       </section>
-      <section className="ServicesContainer">
+      <section className="ServicesContainer" id="nos-services">
         <h2 className="Title">Nos services</h2>
         <p>Nous organisons des événements sur mesure partout dans le monde</p>
         <div className="ListContainer">
@@ -53,11 +54,11 @@ const Page = () => {
           </ServiceCard>
         </div>
       </section>
-      <section className="EventsContainer">
+      <section className="EventsContainer" id="nos-realisations">
         <h2 className="Title">Nos réalisations</h2>
         <EventList />
       </section>
-      <section className="PeoplesContainer">
+      <section className="PeoplesContainer" id="notre-equipe">
         <h2 className="Title">Notre équipe</h2>
         <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
         <div className="ListContainer">
